454b42d9b9d695319b92212b3fec02893b9be4ae
lifecracker8 <bhimpdsharma@gmail.com>
2020-01-13 16:23:25 +0545

    inital commit

---
+++ TestQueConnect/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QueueConnect;
+
+namespace TestQueConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+             * These settings are best to be read from config file; Url & Serviceid
+             */
+            string serverIp = "10.10.10.171"; //url to be connected to the queue machine
+            string db = "UniQUEUE"; //database to connect 
+            int serviceId = 2; //Service id set by the queue system; i,e Deposit,Cash..etc.
+            try
+            {
+                Queue q = new Queue();
+
+                /*
+                 * @input string ip, string db, int serviceId; Mandatory
+                 * @returns  <QueueNo>##<ComeTime>
+                 * Or throws exceptions
+                 */
+                String result = q.getQueue(serverIp,db,serviceId);
+                Console.WriteLine("Raw Output: "+result); // raw result
+                String[] data = result.Split(new string[] { "##" }, StringSplitOptions.None);
+                Console.WriteLine(data[0]); // queue number
+                Console.WriteLine(data[1]); // Come time
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
